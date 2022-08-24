fbfe4cafa56ef48d53177b727526a132c1d8e713
Rishabh jais <rj4rishabhjais.com>
2022-08-24 20:10:31 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      hekko
-    </div>
+    <Switch>
+      <Route exact path="/">
+        Tsi is hilem pahgoe
+      </Route>
+
+      <Route  path="/starr">
+        This is exact path 88888888
+      </Route>
+      <Route> Sorry 404!</Route>
+    </Switch>
   );
 }
 
